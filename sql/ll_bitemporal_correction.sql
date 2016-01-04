b1b7dea4f454f74da2e14e0cff3cf38931bb6ce2
Henrietta Dombrovskaya <henr33@gmail.com>
2016-01-03 22:18:10 -0600

    multi[le fixes and 2 new functions

--- sql/ll_bitemporal_correction.sql
+++ sql/ll_bitemporal_correction.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION ll_bitemporal_correction(p_table text
+CREATE OR REPLACE FUNCTION bitemporal_internal.ll_bitemporal_correction(p_table text
 , p_list_of_fields text -- fields to update
 , p_list_of_values TEXT  -- values to update with
 , p_search_fields TEXT  -- search fields
@@ -11,17 +11,20 @@ $BODY$
 DECLARE
   v_list_of_fields text          :=' ';
   v_list_of_values text          :=' ';
-  v_list_of_fields_to_insert text:=' ';
+  v_list_of_fields_to_insert text;
   v_table_attr text[];
   v_now timestamptz              :=now();-- so that we can reference this time as a constant
   i integer                      := 1;
 BEGIN
-  v_table_attr := ll_bitemporal_list_of_fields(p_table);
+ v_table_attr := bitemporal_internal.ll_bitemporal_list_of_fields(p_table);
+ IF  array_length(v_table_attr,1)=0
+      THEN RAISE EXCEPTION 'Empty list of fields for a table: %', p_table; 
+  RETURN;
+ END IF;
 
-  -- assert array_length(v_list_of_fields_to_insert,1) > 1 
-  v_list_of_fields_to_insert:= array_to_string(v_table_attr, ',','');
+ v_list_of_fields_to_insert:= array_to_string(v_table_attr, ',','');
 
- EXECUTE format($u$ UPDATE %I SET asserted = tstzrange(lower(asserted), %L, '[)')
+ EXECUTE format($u$ UPDATE %s SET asserted = tstzrange(lower(asserted), %L, '[)')
                     WHERE ( %s )=( %s ) AND effective = %L
                           AND upper(asserted)='infinity' $u$  --end assertion period for the old record(s)
           , p_table
@@ -30,9 +33,9 @@ BEGIN
           , p_search_values
           , p_effective);
 
- EXECUTE format($i$INSERT INTO %I ( %s, asserted )
-                SELECT %s , tstzrange(upper(asserted), 'infinity', '[)')
-                  FROM %I WHERE ( %s )=( %s ) AND effective = %L
+ EXECUTE format($i$INSERT INTO %s ( %s, effective, asserted )
+                SELECT %s ,effective, tstzrange(upper(asserted), 'infinity', '[)')
+                  FROM %s WHERE ( %s )=( %s ) AND effective = %L
                           AND upper(asserted)= %L $i$  --insert new assertion rage with old values 
           , p_table
           , v_list_of_fields_to_insert
@@ -44,7 +47,7 @@ BEGIN
           , v_now
 );
 
-    EXECUTE format($uu$UPDATE %I SET ( %s ) = ( %s ) WHERE ( %s ) = ( %s )
+    EXECUTE format($uu$UPDATE %s SET ( %s ) = ( %s ) WHERE ( %s ) = ( %s )
                            AND effective = %L
                            AND upper(asserted)='infinity'$uu$  --update new assertion rage with new values
           , p_table
--- sql/ll_is_bitemporal_table.sql
+++ sql/ll_is_bitemporal_table.sql
@@ -1,24 +1,16 @@
-CREATE OR REPLACE FUNCTION ll_is_bitemporal_table(p_table text) RETURNS boolean   immutable
+CREATE OR REPLACE FUNCTION bitemporal_internal.ll_is_bitemporal_table(p_table text) RETURNS boolean   immutable
 AS
  $$
 DECLARE 
---inp_param_array varchar(50) array;
 v_schemaname text;
 v_tablename text;
---v_sql_final text;
 BEGIN  
---SELECT regexp_split_to_array(v_input_string, ' ') INTO inp_param_array;
-IF  position('.' in p_table) =0 
-   THEN v_schemaname:='public'; 
-   			v_tablename:=p_table;
-   ELSE 			
         SELECT coalesce(split_part(p_table, '.', 1), 'public') INTO v_schemaname;
         SELECT split_part(p_table, '.', 2) INTO v_tablename;
-   END IF;
  RETURN 
  (SELECT 
-         coalesce(max(CASE WHEN t.typname='tsrange' AND attname='asserted' THEN 1 ELSE 0 END),0) +
-         coalesce(max(CASE WHEN t.typname='tsrange' AND attname='effective' THEN 1 ELSE 0 END),0)=2
+         coalesce(max(CASE WHEN t.typname in('tsrange','tstzrange') AND attname='asserted' THEN 1 ELSE 0 END),0) +
+         coalesce(max(CASE WHEN t.typname in('tsrange','tstzrange') AND attname='effective' THEN 1 ELSE 0 END),0)=2
          AND
          bool_and(indisexclusion) AND  bool_and(amname='gist')
   FROM pg_class c 
