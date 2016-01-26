1cc7ffe466b7c039c034ac7eb2e1b1f40f2cfa44
Henrietta Dombrovskaya <henr33@gmail.com>
2016-01-26 15:18:25 -0600

    insert now returns record

--- sql/ll_bitemporal_insert.sql
+++ sql/ll_bitemporal_insert.sql
@@ -1,19 +1,21 @@
 CREATE OR REPLACE FUNCTION bitemporal_internal.ll_bitemporal_insert(p_table text
 ,p_list_of_fields text
 ,p_list_of_values TEXT
-,p_effective tsrange
-,p_asserted tsrange) 
-RETURNS void
+,p_effective temporal_relationships.timeperiod 
+,p_asserted temporal_relationships.timeperiod ) 
+RETURNS RECORD
 AS
  $BODY$
+DECLARE
+v_record RECORD;
 BEGIN
-   EXECUTE format ($i$INSERT INTO %s (%s, effective, asserted )  
-                 VALUES (%s,%L,%L) $i$
+ EXECUTE format ($i$INSERT INTO %s (%s, effective, asserted )  
+                 VALUES (%s,%L,%L) RETURNING * $i$
                 ,p_table
                 ,p_list_of_fields
                 ,p_list_of_values
                 ,p_effective
-                ,p_asserted)
-                ;
+                ,p_asserted) INTO v_record;
+      RETURN v_record;          
      END;    
 $BODY$ LANGUAGE plpgsql;
