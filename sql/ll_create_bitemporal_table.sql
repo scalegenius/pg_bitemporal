d32632cfbc5df875a09b4edca30ef8788ac54891
Henrietta Dombrovskaya <henr33@gmail.com>
2016-04-11 19:57:27 -0500

    fixed the typo in create bitemporal table and changed the test

--- sql/ll_create_bitemporal_table.sql
+++ sql/ll_create_bitemporal_table.sql
@@ -1,4 +1,5 @@
-CREATE OR REPLACE FUNCTION bitemporal_internal.ll_create_bitemporal_table(p_table text, p_table_definition text, p_business_key text) 
+CREATE OR REPLACE FUNCTION bitemporal_internal.ll_create_bitemporal_table(p_schema text,
+p_table text, p_table_definition text, p_business_key text) 
 RETURNS BOOLEAN 
 AS
  $$
@@ -6,19 +7,20 @@ DECLARE
 v_business_key_name text;
 v_business_key_gist text;
 BEGIN
-v_business_key_name :=translate(p_business_key, ', ','_')||'_asserted_effective_excl';
+v_business_key_name :=p_table||translate(p_business_key, ', ','_')||'_assert_eff_excl';
 v_business_key_gist :=replace(p_business_key, ',',' WITH =,')||' WITH =, asserted WITH &&, effective WITH &&';
 --raise notice 'gist %',v_business_key_gist;
 EXECUTE format($create$
-CREATE TABLE %s (
+CREATE TABLE %s.%s (
                  %s
                  ,effective temporal_relationships.timeperiod
                  ,asserted temporal_relationships.timeperiod
                  ,row_created_at timestamptz NOT NULL DEFAULT now()
                  ,CONSTRAINT %s EXCLUDE 
                    USING gist (%s)
-                 ,CONSTRAINT   )
+                    )
                  $create$
+                 ,p_schema
                  ,p_table
                  ,p_table_definition
                  ,v_business_key_name
