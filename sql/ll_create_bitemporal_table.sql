dbea4d19d136e822dbdf85ad6c464e71a9383d15
hettie-d <henr33@gmail.com>
2016-09-18 22:50:38 -0500

    changes to create bitemp table
    changes to create bitemp table: force serial key, fix the constraint
    name

--- sql/ll_create_bitemporal_table.sql
+++ sql/ll_create_bitemporal_table.sql
@@ -1,28 +1,40 @@
-CREATE OR REPLACE FUNCTION bitemporal_internal.ll_create_bitemporal_table(p_table text, p_table_definition text, p_business_key text) 
-RETURNS BOOLEAN 
-AS
- $$
+CREATE OR REPLACE FUNCTION bitemporal_internal.ll_create_bitemporal_table(
+    p_schema text,
+    p_table text,
+    p_table_definition text,
+    p_business_key text)
+  RETURNS boolean AS
+$BODY$
 DECLARE 
 v_business_key_name text;
 v_business_key_gist text;
+v_serial_key_name text;
 BEGIN
-v_business_key_name :=translate(p_business_key, ', ','_')||'_asserted_effective_excl';
+v_serial_key_name :=p_table||'_key serial';
+v_business_key_name :=p_table||'_'||translate(p_business_key, ', ','_')||'_assert_eff_excl';
 v_business_key_gist :=replace(p_business_key, ',',' WITH =,')||' WITH =, asserted WITH &&, effective WITH &&';
 --raise notice 'gist %',v_business_key_gist;
 EXECUTE format($create$
-CREATE TABLE %s (
+CREATE TABLE %s.%s (
                  %s
+                 ,%s
                  ,effective temporal_relationships.timeperiod
                  ,asserted temporal_relationships.timeperiod
                  ,row_created_at timestamptz NOT NULL DEFAULT now()
                  ,CONSTRAINT %s EXCLUDE 
-                   USING gist (%s))
+                   USING gist (%s)
+                    )
                  $create$
+                 ,p_schema
                  ,p_table
+                 ,v_serial_key_name
                  ,p_table_definition
                  ,v_business_key_name
                  ,v_business_key_gist
                  ) ;
- RETURN ('true');               
+ RETURN ('true');  
+ EXCEPTION WHEN OTHERS THEN
+RETURN ('false');             
 END;
-$$ LANGUAGE plpgsql;
+$BODY$
+  LANGUAGE plpgsql;
