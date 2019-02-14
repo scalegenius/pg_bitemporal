2a1d50095ac3aec6f397ee5ddf82ee310dc6c8ff
hettie-d <henr33@gmail.com>
2019-02-14 14:10:13 -0600

    correction fix to accommodate corner cases

--- sql/ll_bitemporal_correction.sql
+++ sql/ll_bitemporal_correction.sql
@@ -120,6 +120,7 @@ v_sql  text;
   v_serial_key text:=p_table_name||'_key';
   v_table text:=p_schema_name||'.'||p_table_name;
   v_keys int[];
+  v_keys_old  int[];
 BEGIN
  v_table_attr := bitemporal_internal.ll_bitemporal_list_of_fields(v_table);
  IF  array_length(v_table_attr,1)=0
@@ -129,41 +130,57 @@ BEGIN
 
  v_list_of_fields_to_insert:= array_to_string(v_table_attr, ',','');
  EXECUTE 
- format($u$ UPDATE %s SET asserted = temporal_relationships.timeperiod_range(lower(asserted), %L, '[)')
+ format($u$ WITH updt AS (UPDATE %s SET asserted = temporal_relationships.timeperiod_range(lower(asserted), %L, '[)')
                     WHERE ( %s )=( %s ) AND effective = %L
                           AND upper(asserted)='infinity' 
-                          AnD lower(asserted)<%L$u$  --end assertion period for the old record(s), if any
+                          AnD lower(asserted)<%L returning %s )
+                                      SELECT array_agg(%s) FROM updt
+                                      $u$  --end assertion period for the old record(s), if any
           , v_table
           , v_now
           , p_search_fields
           , p_search_values
           , p_effective
-          , v_now);
+          , v_now
+          , v_serial_key
+          , v_serial_key) into v_keys_old;
  --       raise notice 'sql%', v_sql;  
 
  EXECUTE 
 -- v_sql:=
  format($i$WITH inst AS (INSERT INTO %s ( %s, effective, asserted )
                 SELECT %s ,effective, temporal_relationships.timeperiod_range(upper(asserted), 'infinity', '[)')
-                  FROM %s WHERE ( %s )=( %s ) AND effective = %L
-                          AND upper(asserted)= %L 
+                  FROM %s WHERE ( %s )IN ( %s ) 
                                 returning %s )
                                     SELECT array_agg(%s) FROM inst $i$  --insert new assertion rage with old values where applicable 
           , v_table
           , v_list_of_fields_to_insert
           , v_list_of_fields_to_insert
           , v_table
-          , p_search_fields
-          , p_search_values
-          , p_effective
-          , v_now
-          ,v_serial_key
-          ,v_serial_key
+          , v_serial_key
+          , coalesce(array_to_string(v_keys_old,','),'NULL')
+          , v_serial_key
+          , v_serial_key
 )into v_keys;
 --raise notice 'sql%', v_sql;  
 
 --raise notice 'sql%', v_sql;  
+if coalesce(array_to_string(v_keys_old,',')) IS NULL 
+   then 
+EXECUTE   format($uu$UPDATE %s SET ( %s ) = (SELECT %s ) WHERE ( %s ) = ( %s )
+                           AND effective = %L
+                           AND upper(asserted)='infinity'
+                            $uu$  --update new assertion rage with new values
+          , v_table
+          , p_list_of_fields
+          , p_list_of_values
+          , p_search_fields
+          , p_search_values
+          , p_effective
+     ) 
+	;
 
+ELSE 
  EXECUTE 
 -- v_sql:=   
  format($uu$UPDATE %s SET ( %s ) = ( SELECT %s ) WHERE ( %s ) IN ( %s )
@@ -174,7 +191,8 @@ BEGIN
           , v_serial_key
           ,coalesce(array_to_string(v_keys,','), 'NULL'));
           
-   --  raise notice 'sql%', v_sql;  
+   --  raise notice 'sql%', v_sql; 
+ END IF;  
  GET DIAGNOSTICS v_rowcount:=ROW_COUNT; 
 
  RETURN v_rowcount;
@@ -182,6 +200,7 @@ END;
 $BODY$
   LANGUAGE plpgsql VOLATILE; 
   
+
  
  CREATE OR REPLACE FUNCTION bitemporal_internal.ll_bitemporal_correction(p_schema_name text,
     p_table_name text,
