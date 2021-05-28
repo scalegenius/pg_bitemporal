0bcd96e0893a9f33703045943483f42c1ac4c73e
Chad Slaughter <chad@brokerxapp.com>
2021-05-28 13:00:58 -0500

    clean up circleci config
    - log that we are skipping reload test and why
    - remove error causing psql command

--- build/test_database.sh
+++ build/test_database.sh
@@ -11,6 +11,8 @@ ${BUILD}/pg_prove --ext .pg --ext .sql ${APP_ROOT}/tests/
 PGTAP_TEST=$?
 
 echo "Test if load_all is reloadable."
+# TODO: make new 'create type" reloadable
+echo "There will be an ERROR for type bitemporal_pg_constraint"
 echo "\\set CI $CI %\\ir $D/_load_all.sql" |tr '%' '\n' | $PSQL
 RELOAD_TEST=$?
 
@@ -19,6 +21,7 @@ echo " "
 echo "pgtap test returned:" $PGTAP_TEST
 echo "reload test returned:" $RELOAD_TEST
 
+echo "Skipping RELOAD_TEST Check"
 # if ((  $PGTAP_TEST || $RELOAD_TEST )); then
 if ((  $PGTAP_TEST )); then
   exit 1
